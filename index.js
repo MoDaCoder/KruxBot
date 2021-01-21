6fdaa144cfee5604edffa4d5cd4727dbb27a2d45
mohehexd <dumblemoe@gmail.com>
2021-01-20 18:52:20 -0600

    Creating basic variables with dummy data.

---
+++ index.js
@@ -0,0 +1,66 @@
+// Example of Sneaker Bot
+
+let searchKeyword = '+Jordan';
+
+// +high-top, +mid
+
+let catogories = [
+    
+"Basketball",      // ->0
+
+"Running",         // ->1
+
+"Lifestyle",       // ->2
+
+"Sandals/Slides",  // -3
+
+"Boots",           // ->4
+
+];
+
+let category = catgories[4];
+
+// You could edit it for which one you want.
+
+let prefferedSize = 'any'
+
+let preferColor = '';
+
+let checkout_delay = 1000;
+
+
+// adress info
+
+let billing_name = "John Doe";
+
+let billing_email= "john.doe@gmail.com";
+
+let billing_phone_number = "1234567890";
+
+let billing_address = "123 Main Street";
+
+let billing_adress_2= "";
+
+let billing_zip = "11223";
+
+let billing_city = "New York";
+
+let billing_state = "NY";
+
+let billing_country = "USA"
+
+
+// Payment info
+
+let credit_card_type = "Visa";
+
+Let_credit_card_number = " 0000 1111 2222 3333";
+
+let credit_card_month = "01";
+
+let credit_card_year = "2019";
+
+let credit_card_cvv = "001";
+
+// All values in the " " can be changed to match your details
+
